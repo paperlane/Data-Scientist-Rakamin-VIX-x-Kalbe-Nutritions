962df123a887006ed08e068e8cd9d80c2be87366
Muh Reynaldi <129879580+paperlane@users.noreply.github.com>
2023-07-30 19:22:40 +0700

    Add files via upload

---
+++ Dbeaveur PostgreSQL/Script.sql
@@ -0,0 +1,35 @@
+
+-- Average Age based on Marital Status
+SELECT "Marital Status" , ROUND(AVG(age)) AS average_age
+FROM "fpKalbe".customer c
+where "Marital Status" IS NOT NULL 
+AND "Marital Status" <> ''
+GROUP BY "Marital Status" ;
+
+-- Average Age based on Gender
+select 
+	case 
+		when gender = 0 then 'Female'
+		when gender = 1 then 'Male'
+		else 'Unknown'
+	end as gender,
+	ROUND(avg(age)) as average_age
+from "fpKalbe".customer c  
+where gender is not null 
+group by gender
+
+-- Store Name with Highest quantity
+select s.storename, sum(qty) as total_quantity
+from "fpKalbe".store s
+join "fpKalbe"."transaction" t on s.storeid = t.storeid 
+group by s.storename 
+order by total_quantity desc 
+limit 1 ;
+
+-- Product Name with Highest Selling
+select p."Product Name" , sum(totalamount) as total_amount, sum(qty) as total_quantity
+from "fpKalbe".product p 
+join "fpKalbe"."transaction" t on p.productid = t.productid 
+group by p."Product Name" 
+order by total_amount desc 
+limit 1 ;
